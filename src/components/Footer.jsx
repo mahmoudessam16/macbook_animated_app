9fee36296bc423e5b740ca53df219a1492053735
mahmoudessam16 <mahmoudemourad29@gmail.com>
2026-01-02 23:00:48 +0200

    Implement Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
 const Footer = () => {
-  return <div></div>;
+  return <footer>
+    <div className="info">
+      <p>More ways to shop: {" "}
+        <span>Find an Apple Store</span>
+        {" "} 
+        or 
+        {" "}
+        <span>other retailer</span>
+        {" "} 
+        near you. Or call 000800 040 1966
+      </p>
+    </div>
+
+    <hr />
+
+      <div className="links">
+        <p>Copyright &copy; 2025</p>
+        <ul>
+          <li>Privacy Policy</li>
+          <li>Terms of Use</li>
+          <li>Sales Policy</li>
+          <li>Legal</li>
+          <li>Site Map</li>
+        </ul>
+      </div>
+  </footer>;
 };
 
 export default Footer;
